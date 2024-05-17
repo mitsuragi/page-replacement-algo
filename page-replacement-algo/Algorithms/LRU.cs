7e0fbcd05f8d7ee6153df51cf616a9348414ecb1
qweasdzxc <mitsuri66it@gmail.com>
2024-05-17 14:43:15 +0300

    init commit

---
+++ page-replacement-algo/Algorithms/LRU.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace page_replacement_algo.Algorithms
+{
+    public class LRU
+    {
+        public static int CountInterruptions(int[] appeals, List<int> pb, bool flag)
+        {
+            List<int> pageBlocks = new List<int>(pb);
+            int counter = 0;
+
+            if(flag)
+            {
+                for(int i = 0; i < appeals.Length; i++)
+                {
+                    if (pageBlocks.Contains(appeals[i]))
+                    {
+                        pageBlocks.RemoveAt(pageBlocks.IndexOf(appeals[i]));
+                        pageBlocks.Add(appeals[i]);
+
+                        Console.Write("Обращение к " + appeals[i] + ": Успешно\t");
+                        Console.Write("\tСтраничные блоки: ");
+                        displayPageBlocks();
+                        Console.WriteLine();
+                        continue;
+                    }
+                    else
+                    {
+                        Console.Write("Обращение к " + appeals[i]);
+                        Console.Write(": Прерывание\t");
+                        Console.Write("Было -> ");
+                        displayPageBlocks();
+                        pageBlocks.Add(appeals[i]);
+                        pageBlocks.RemoveAt(0);
+                        Console.Write("\tСтало -> ");
+                        displayPageBlocks();
+                        Console.WriteLine();
+                        counter++;
+                    }
+                }
+            }
+            else
+            {
+                foreach (int i in appeals)
+                {
+                    if (pageBlocks.Contains(i))
+                    {
+                        pageBlocks.RemoveAt(pageBlocks.IndexOf(i));
+                        pageBlocks.Add(i);
+
+                        continue;
+                    }
+                    else
+                    {
+                        pageBlocks.Add(i);
+                        pageBlocks.RemoveAt(0);
+                        counter++;
+                    }
+                }
+
+            }
+
+            void displayPageBlocks()
+            {
+                foreach (int i in pageBlocks)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ page-replacement-algo/Program.cs
@@ -0,0 +1,96 @@
+﻿using page_replacement_algo.Algorithms;
+
+namespace page_replacement_algo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] appeals = { 7, 8, 9, 2, 1, 0, 8, 9, 2, 4, 6, 8, 2, 1, 8, 9 };
+            List<int> pageBlocks = new List<int> { 8, 2, 9, 6 };
+            bool flag = false;
+            bool quit = false;
+            int input;
+
+            Console.WriteLine("1 - Display Appeals\n" +
+                              "2 - Display PageBlocks\n" +
+                              "3 - Add Page Block\n" +
+                              "4 - Remove Page Block\n" +
+                              "5 - Switch Flag\n" +
+                              "6 - FIFO Interruptions\n" +
+                              "7 - LRU Interruptions\n" +
+                              "0 - Quit\n" +
+                              "Flag status - " + flag);
+
+            while(!quit)
+            {
+                try
+                {
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                switch(input)
+                {
+                    case 1:
+                        Console.WriteLine("Appeals");
+                        foreach(int i in appeals)
+                        {
+                            Console.Write(i + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("PageBlocks");
+                        foreach(int i in pageBlocks)
+                        {
+                            Console.Write(i + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        pageBlocks.Insert(0, -1);
+                        Console.WriteLine("Page Block Added");
+                        break;
+
+                    case 4:
+                        if (pageBlocks.Count > 0)
+                        {
+                            pageBlocks.RemoveAt(0);
+                            Console.WriteLine("Page Block Removed");
+                        }
+                        else
+                        {
+                            Console.WriteLine("PageBlocks is already clear");
+                        }
+                        break;
+
+                    case 5:
+                        if (flag) flag = false;
+                        else flag = true;
+                        Console.WriteLine("Flag Status: " + flag);
+                        break;
+
+                    case 6:
+                        Console.WriteLine("FIFO Interruptions: " + FIFO.CountInterruptions(appeals, pageBlocks, flag));
+                        break;
+
+                    case 7:
+                        Console.WriteLine("LRU Interruptions: " + LRU.CountInterruptions(appeals, pageBlocks, flag));
+                        break;
+
+                    case 0:
+                        quit = true;
+                        break;
+                }
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}
